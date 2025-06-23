138ae358906a08c470c60beb669c97cab67008b0
Russian kid <158831528+Russiankids@users.noreply.github.com>
2025-06-23 20:19:02 +0300

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,10 @@
+# Discord token [string]
+TOKEN = 'MTM4Njc1MTE5ODcxOTI0NjUzMA.G4IS7W.gGBbt4y2gsRbtZomIAUKNNqEniteQZnDJ1pnv4'
+# Server id [int]
+SERVER = 1350592351965417613
+# From channels [list<int>]
+FROM_CHANNELS = [1355584987176632550, 1355587374951829635]
+# To channel [int]
+TO_CHANNEL = 1386753324077809774
+# whether to show Discord message and log on the terminal [False/True]
+Verbose = True
